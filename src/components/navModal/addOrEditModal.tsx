3761a7c6d5625fc4cc60ce2fdd0fb6770c6b1eb3
SpectreAlan <comjszoo@gmail.com>
2023-10-12 21:56:31 +0700

    add nav edit modal

---
+++ src/components/navModal/addOrEditModal.tsx
@@ -0,0 +1,13 @@
+import React, {useEffect, useState} from 'react'
+
+interface IProps {
+    setModalVisible: (boolean)=>void
+    info: any
+}
+
+const AddOrEditModal:React.FC<IProps> = ({setModalVisible, info})=>{
+
+    
+}
+
+export default AddOrEditModal
