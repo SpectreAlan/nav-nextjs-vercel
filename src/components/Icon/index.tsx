38df149c70ab8efbd556a603c9d3283c2ba983b6
SpectreAlan <comjszoo@gmail.com>
2023-10-24 19:48:13 +0800

    update iconfont

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,7 +1,7 @@
 import dynamic from 'next/dynamic';
 
 const Icon = dynamic(() => import('@ant-design/icons').then(icons => icons.createFromIconfontCN({
-    scriptUrl: '//at.alicdn.com/t/c/font_4220832_f80xshuj6ej.js',
+    scriptUrl: '//at.alicdn.com/t/c/font_4220832_0toeje2v2lld.js',
 })), { ssr: false });
 
 export default Icon
