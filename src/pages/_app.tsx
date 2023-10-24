c2d7d8c90a4b3129d1557f6d30b06b3f2452b6cb
SpectreAlan <comjszoo@gmail.com>
2023-10-24 18:19:53 +0800

    add ,ianfest file

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React  from 'react'
 import {GlobalProvider} from '@/GlobalContext'
 import {SessionProvider} from "next-auth/react"
 import {AppProps} from "next/app";
@@ -6,7 +6,6 @@ import '../static/style/global.scss'
 import Layout from "@/components/layout";
 import theme from '../../theme/themeConfig';
 import {ConfigProvider} from 'antd';
-import Head from "next/head";
 
 const App = ({Component, pageProps}: AppProps) => {
 
@@ -14,9 +13,6 @@ const App = ({Component, pageProps}: AppProps) => {
         <SessionProvider session={pageProps.session}>
             <GlobalProvider>
                 <ConfigProvider theme={theme}>
-                    <Head>
-                        <link rel='shortcut icon' type='image/x-icon' href='/favicon.ico' />
-                    </Head>
                     <Layout>
                         <Component {...pageProps} />
                     </Layout>
