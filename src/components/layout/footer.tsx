a1a009bdbea0ae182b7a600261a2b73cb0e73478
SpectreAlan <comjszoo@gmail.com>
2023-10-28 13:29:47 +0800

    edit footer

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -3,7 +3,7 @@ import React, {useState} from 'react'
 export default () => {
     const [info, setInfo] = useState({total: 0, today: 0})
     return <div className='py-1 text-center border-t-grey'>
-        <div>Powered by SpectreAlan 访客(总数/今日): {info.today} / {info.today}</div>
+        <div>Powered by <a href="https://github.com/SpectreAlan" target='_blank'>SpectreAlan</a> 访客(今日/总数): {info.today} / {info.total}</div>
         <img src="https://nav-vercel.oss-cn-hongkong.aliyuncs.com/base/dance.gif" alt="dance" className='w-10'/>
     </div>
 }
