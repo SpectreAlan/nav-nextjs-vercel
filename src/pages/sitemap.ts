1789dfc18e3158563935e361afcdcc44d95c5922
SpectreAlan <comjszoo@gmail.com>
2023-11-11 21:49:36 +0700

    add sitemap

--- src/pages/sitemap.ts
+++ src/pages/sitemap.ts
@@ -4,21 +4,15 @@ export default function sitemap(): MetadataRoute.Sitemap {
     return [
         {
             url: 'https://efv.cc',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 1,
+            lastModified: new Date()
         },
         {
             url: 'https://efv.cc/post',
             lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.8,
         },
         {
             url: 'https://efv.cc/clipboard',
-            lastModified: new Date(),
-            changeFrequency: 'weekly',
-            priority: 0.5,
+            lastModified: new Date()
         },
     ]
 }
